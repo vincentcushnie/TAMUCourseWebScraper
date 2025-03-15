3422bd557b618204a01536752063eb1c8fc04841
vincentcushnie <vincent.cushnie@tamu.edu>
2025-03-15 16:39:11 -0500

    degrees can now be scraped. Many of the rules are just saved as text, rather than encoded to allow algorithmic checking since this would have taken an exorbitant amount of time due to a lack of standardization of language. Aside from a few small fixes, the data scraper is finished.

--- functions.cpp
+++ functions.cpp
@@ -12,7 +12,7 @@ size_t Functions::WriteCallback(void* contents, size_t size, size_t nmemb, void*
     return size * nmemb;
 }
 
-std::string Functions::trimUrl(const std::string& url) {
+std::string Functions::getMajorFromUrl(const std::string& url) {
     size_t lastSlash = url.find_last_of('/');
     size_t secondLastSlash = url.find_last_of('/', lastSlash - 1);
 
@@ -23,6 +23,20 @@ std::string Functions::trimUrl(const std::string& url) {
     std::string trimmed = url.substr(secondLastSlash + 1, lastSlash - secondLastSlash - 1);
     return trimmed;
 }
+
+std::string Functions::getDepartmentFromUrl(const std::string& url) {
+    size_t lastSlash = url.find_last_of('/');
+     lastSlash = url.find_last_of('/', lastSlash - 1);
+    size_t secondLastSlash = url.find_last_of('/', lastSlash - 1);
+
+    if (secondLastSlash == std::string::npos || lastSlash == std::string::npos) {
+        return "invalid"; // Handle unexpected format
+    }
+
+    std::string trimmed = url.substr(secondLastSlash + 1, lastSlash - secondLastSlash - 1);
+    return trimmed;
+}
+
 
 std::vector<std::string> Functions::extractCodes(const std::string &text) {
     std::regex pattern(R"([A-Z]{4}\d{3})"); // Matches four capital letters followed by three digits
@@ -98,10 +112,10 @@ void Functions::curlRequest(std::string scrapingUrl){
 
     curl_easy_cleanup(curl);
     file.close();
-    std::cout << "HTML saved to raw.html" << std::endl;
+    //std::cout << "HTML saved to raw.html" << std::endl;
     std::string command = "tidy -q -utf8 -asxml -o cleaned.html raw.html 2>/dev/null";
     system(command.c_str());
-    std::cout << "Cleaned HTML saved to cleaned.html" << std::endl;
+    //std::cout << "Cleaned HTML saved to cleaned.html" << std::endl;
 }
 
 std::vector<std::string> Functions::courseInformationScrape(pugi::xml_document& doc){
@@ -125,14 +139,11 @@ std::vector<std::string> Functions::courseInformationScrape(pugi::xml_document& 
 }
 
 void Functions::courseInformationProcess(std::string coursesUrl, std::vector<std::string>& data){
-    std::string trimmedUrl = Functions::trimUrl(coursesUrl);
-    std::string filename = trimmedUrl + "-courses.txt";
-    std::ofstream courseTable(trimmedUrl + "CourseTable.csv");
-    std::ofstream prereqTable(trimmedUrl + "PrereqTable.csv");
-    std::ofstream crossListingTable(trimmedUrl + "CrossListingTable.csv");
-    courseTable<<"Course Code, Course Name, Field, Credits, Lecture, Lab, Description, Difficulty"<<std::endl;
-    prereqTable<<"Course Code, Course Code, Grade Req, Group, Text, Concurrent Enrollment"<<std::endl;
-    crossListingTable<<"Course Code, Course Code"<<std::endl;
+    std::string trimmedUrl = Functions::getMajorFromUrl(coursesUrl);
+    std::ofstream courseTable("coursesTable.csv", std::ios::app);
+    std::ofstream prereqTable("coursesPrerequisitesTable.csv", std::ios::app);
+    std::ofstream crossListingTable("coursesCrossListingTable.csv", std::ios::app);
+    
     std::string currentCourse="";
     for(std::string row: data){
         while(row.find("&nbsp;")!=std::string::npos){
@@ -256,8 +267,6 @@ void Functions::prerequisiteProcess(std::ofstream& prereqTable, std::string row,
 void Functions::crossListingProcess(std::ofstream& crossListingTable, std::string row, std::string currentCourse){
     if(row.find("Cross Listing")!=std::string::npos){
         row=row.substr(row.find("Cross Listing")+13);
-        std::cout<<row<<std::endl;
-
         std::vector<std::string> equivalents = Functions::extractCodes(row);
         for(std::string a: equivalents){
             if(currentCourse!=a){
@@ -273,5 +282,222 @@ void Functions::removeNewlines(std::string& input) {
         if (c == '\n' || c == '\r') {
             c = ' ';
         }
+    }
+}
+
+void Functions::extract_text(const pugi::xml_node& node, std::string& result) {
+    for (auto child : node.children()) {
+        if (child.type() == pugi::node_pcdata) {
+            result += child.value();
+        } else if (child.type() == pugi::node_element) {
+            extract_text(child, result); // Recursively extract text from elements
+        }
+    }
+}
+
+void Functions::replaceNbsp(std::string& text, std::string repl) {
+    const std::string target = "&nbsp;";
+    const std::string replacement = repl;
+    size_t pos = 0;
+
+    while ((pos = text.find(target, pos)) != std::string::npos) {
+        text.replace(pos, target.length(), replacement);
+    }
+}
+
+
+
+
+void Functions::degreeInformationProcessAndScrape(pugi::xml_document& doc, int id, std::string degreeUrl){
+    std::vector<std::string> data;
+    std::vector<std::string> courseSuperscripts;
+    int majorID=id;
+    auto title = doc.child("html").child("head").child("title");
+    std::string majorName=std::string(title.text().get());
+    majorName=majorName.substr(0, majorName.find('<')-1);
+    std::string majorCode="N/A";
+    std::string department=Functions::getDepartmentFromUrl(degreeUrl);
+    int credits=0;
+    int difficulty=0;
+    std::string majorRules="[";
+    for(pugi::xml_node node_1 : doc.child("html").child("body").child("section").child("div").children("div")){
+        if (std::string(node_1.attribute("id").value())=="col-content"){
+            for(pugi::xml_node node_2 : node_1.child("main").children("div")){
+                if(std::string(node_2.attribute("id").value())=="programrequirementstextcontainer"){
+                    Functions::processMajorRules(node_2, majorRules);
+                    Functions::processCourseSuperscripts(node_2, courseSuperscripts);
+                    Functions::processCourseTables(node_2, courseSuperscripts, data, id, credits);
+                }
+            }
+        }
+    }
+    std::ofstream majorFile("majorTable.csv", std::ios::app);
+    majorFile<<std::to_string(majorID)<<","<< majorName <<","<< majorCode<<","<<department<<","<<std::to_string(credits)<<","<<std::to_string(difficulty)<<majorRules<<std::endl;
+    majorFile.close();
+    std::ofstream courseFile("majorToCoursesTable.csv", std::ios::app);
+    for(std::string row: data){
+        if(row.find("&nbsp;",0)!=std::string::npos){
+            row.replace(row.find("&nbsp;",0), 6,"");
+        }
+       // std::cout<<"row: "<<row<<std::endl;
+        courseFile<<row<<std::endl;
+    }
+    courseFile.close();
+}
+
+
+void Functions::processMajorRules(pugi::xml_node& node_2, std::string& majorRules){
+    for(pugi::xml_node node_3: node_2.children("p")){
+        std::string majorRule="";
+        Functions::extract_text(node_3, majorRule);
+        Functions::removeNewlines(majorRule);
+        Functions::replaceNbsp(majorRule, "");
+        majorRules+=majorRule;
+        majorRules+=",";
+    }
+    majorRules[majorRules.length()-1]=']';
+    if(majorRules=="]") majorRules="";
+}
+
+void Functions::processCourseSuperscripts(pugi::xml_node& node_2, std::vector<std::string>& courseSuperscripts){
+    for(pugi::xml_node node_3: node_2.children("dl")){
+        for(pugi::xml_node node_4 : node_3.children("dd")){
+            
+            std::string rule="";
+            Functions::extract_text(node_4.child("p"), rule);
+            Functions::removeNewlines(rule);
+            Functions::replaceNbsp(rule, "");
+            courseSuperscripts.push_back(rule);
+        }
+    }
+}
+
+void Functions::processCourseTables(pugi::xml_node& node_2, std::vector<std::string>& courseSuperscripts, std::vector<std::string>& data, const int& majorID, int& credits){
+    int year=0;
+    int season=1;
+    for(pugi::xml_node node_3: node_2.children("table")){
+        if(std::string(node_3.attribute("class").value())=="sc_plangrid"){
+            int group=0;
+            int hours;
+            for(pugi::xml_node node_4: node_3.children("tr")){
+                std::string nodeClass = std::string(node_4.attribute("class").value());
+                if(nodeClass=="plangridyear"){
+                    year++;
+                }else if(nodeClass=="plangridterm"){
+                    if(season==0){
+                        season=1;
+                    }else{
+                        season=0;
+                    }
+                }else if(nodeClass=="odd" || nodeClass=="even"){
+                    Functions::processCourseRow(node_4, year, season, group, hours, majorID, data, courseSuperscripts);
+                    
+                }else if(nodeClass=="plangridtotal lastrow odd"||nodeClass=="plangridtotal lastrow even"){
+                    for(pugi::xml_node node_5: node_4.children("td")){
+                        if(std::string(node_5.attribute("class").value())=="hourscol"){
+                            if(credits==0){
+                                credits=std::stoi(node_5.text().get());
+                            }else{
+                                credits+=std::stoi(node_5.text().get());
+                            }
+                        }
+                    }
+                }else if(nodeClass=="plangridsum even"||nodeClass=="plangridsum odd"){
+                }
+                else{
+                    data.push_back("no case");
+                }
+            }
+        }
+    }
+}
+
+void Functions::processCourseRow(pugi::xml_node& node_4, int& year, int& season, int& group, int& hours, const int& majorID, std::vector<std::string>& data, std::vector<std::string>& courseSuperscripts){
+    std::string codecol="";
+    std::string codecol2="";
+    std::string titlecol="";
+    std::string titlecol2="";
+    std::string hourscol="";
+    std::string a="";
+    std::string b="";
+    std::string c="";
+    std::string d="";
+    std::string e="";
+    std::string aa="";
+    std::string bb="";
+    std::string aaa="";
+    std::string superscripts="";
+    std::string superscriptsSecond="";
+    for(pugi::xml_node node_5: node_4.children("td")){
+            
+        if(std::string(node_5.attribute("class").value())=="codecol"){                
+                c=node_5.child("div").child("span").child("a").text().get();
+                b=node_5.child("div").child("a").text().get();
+                a=node_5.child("a").text().get();
+                if(a==""){
+                    a=node_5.child("div").child("div").child("a").text().get();
+                }
+                d=node_5.child("span").child("a").text().get();
+                e=node_5.child("span").text().get();
+                superscripts=node_5.child("sup").text().get();
+        }
+        if(std::string(node_5.attribute("class").value())=="titlecol"){
+            aa=node_5.text().get();
+            bb=node_5.child("div").text().get();
+            if(superscripts==""){
+                superscripts=node_5.child("sup").text().get();
+            }
+        }
+        if(std::string(node_5.attribute("class").value())=="hourscol"){
+            aaa=node_5.text().get();
+        }
+    }
+    if(aaa==""){ 
+    }else{
+        hours=std::stoi(aaa);
+        group++;  
+    }
+    if(aa=="" && bb=="" && c==""){
+        if(d=="" && e==""){
+            std::cout<<"something wrong"<<std::endl;
+        }
+        if(d!=""){
+            codecol=d;
+        }
+        if(e!=""){
+            codecol=e;
+        }
+    }else{
+        if(c!=""){
+            codecol=c;
+            titlecol=aa;
+        }else{
+            codecol=a;
+            codecol2=b;
+            titlecol=aa;
+            titlecol2=bb;
+        }
+    }
+    hourscol=hours;
+    Functions::removeNewlines(titlecol);
+    Functions::removeNewlines(titlecol2);
+    Functions::removeNewlines(codecol);
+    Functions::removeNewlines(codecol2);
+    std::stringstream ss(superscripts);
+    std::string token;
+    std::string rules="";
+    while (std::getline(ss, token, ',')) {
+        rules+=courseSuperscripts[stoi(token)-1];
+    }
+    std::stringstream s2(superscriptsSecond);
+    std::string token2;
+    std::string rules2="";
+    while (std::getline(s2, token2, ',')) {
+        std::cout<<"rules: "<<std::endl;
+        rules2+=courseSuperscripts[stoi(token2)-1];
+    }
+    data.push_back(std::to_string(majorID)+","+codecol+","+titlecol+","+std::to_string(group)+","+std::to_string(year*10+season)+",["+rules+"]");
+    if(codecol2!=""){
+        data.push_back(std::to_string(majorID)+","+codecol2+","+titlecol2+","+std::to_string(group)+","+std::to_string(year*10+season)+",["+superscriptsSecond+"]");
     }
 }
--- functions.h
+++ functions.h
@@ -10,16 +10,25 @@
 class Functions {
     public:
         static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
-        static std::string trimUrl(const std::string& url);
+        static std::string getMajorFromUrl(const std::string& url);
+        static std::string getDepartmentFromUrl(const std::string& url);
         static std::vector<std::string> extractCodes(const std::string &text);
         static std::string extract_code(const std::string& text);
         static void split_conditions(const std::string& requirement, std::vector<std::vector<std::string>>& or_groups);
         static void curlRequest(std::string scrapingUrl);
         static std::vector<std::string> courseInformationScrape(pugi::xml_document& doc);
+        static void degreeInformationProcessAndScrape(pugi::xml_document& doc, int id, std::string url);
         static void courseInformationProcess(std::string coursesUrl, std::vector<std::string>& data);
         static void prerequisiteProcess(std::ofstream& prereqTable, std::string row, std::string currentCourse);
         static void crossListingProcess(std::ofstream& crossListingTable, std::string row, std::string currentCourse);
         static void removeNewlines(std::string& input);
+        static void extract_text(const pugi::xml_node& node, std::string& result);
+        static void replaceNbsp(std::string& text, std::string repl);
+        static void processMajorRules(pugi::xml_node& node_2, std::string& majorRules);
+        static void processCourseSuperscripts(pugi::xml_node& node_2, std::vector<std::string>& courseSuperscripts);
+        static void processCourseTables(pugi::xml_node &node_2, std::vector<std::string>& courseSuperscripts, std::vector<std::string> &data, const int &majorID, int &credits);
+        static void processCourseRow(pugi::xml_node& node_4, int& year, int& season, int& group, int& hours, const int& majorID, std::vector<std::string>& data, std::vector<std::string>& courseSuperscripts);
+
     };
 
 
